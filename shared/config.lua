7a09cb6e4f4fe0bb22fe0e1712f9682dd34ac594
NumberSixDev <38400768+NumberSixDev@users.noreply.github.com>
2024-02-26 21:47:10 +0000

    Create config.lua

---
+++ shared/config.lua
@@ -0,0 +1,22 @@
+Config = {}
+
+Config.Rentals = {
+    [1] = 'Sultan',
+    [2] = 'Futo',
+    [3] = 'RapidGT',
+    [4] = 'Banshee',
+}
+
+Config.JobLock = 'police' -- | True - Locked To Job (Example: 'police') | False - No Job Lock |
+
+Config.WarpIntoVehicle = true -- | True - Warp Player Into Rental Vehicle | False - Do Not Warp Player Into Vehicle |
+
+Config.FuelSpawn = 100 -- | How Much Fuel To Spawn The Vehicle With (0-100) |
+
+Config.Headings = {
+    PrisonHeading = 269.0,
+    LegionHeading = 0.0,
+    HospitalHeading = 67.9
+}
+
+Config.RentalReturn = 250 -- | Money You Get When You Return Your Rental |
