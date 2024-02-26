4ab69999e49acdf1de49df591ef2847595363501
NumberSixDev <38400768+NumberSixDev@users.noreply.github.com>
2024-02-26 21:34:08 +0000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'NS Development'
+description 'ns-rentals'
+version '0.0.1'
+
+client_scripts {
+    'client/client.lua'
+}
+server_scripts {
+    'server/server.lua'
+}
+
+shared_scripts {
+    'shared/config.lua'
+}
+
+lua54 'yes'
